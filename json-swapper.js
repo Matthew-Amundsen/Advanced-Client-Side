da3a6f8f825a234071d4f471f6687a68bcc678ee
Matthew Amundsen <ctrlaltdisease@gmail.com>
2015-05-20 11:27:26 +1200

    Final layout. jQuery added for mobile menu. SVG logo.

--- json-swapper.js
+++ json-swapper.js
@@ -26,3 +26,17 @@ $(document).ready(function() {
 		});
 	});
 });
+
+$(document).ready(function() {
+    var $navButton = $("nav button");
+    var $navUl = $("nav ul");
+
+    $navButton.click(function() {
+        $navUl.slideToggle();
+    });
+
+    $(window).resize(function() {
+        $("nav ul").css('display', '');
+    });
+
+});
